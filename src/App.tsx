b5faef25953b14c0fbadcde7a14e61d14b29ed8c
Nicolas Mugnier <6680190+NicolasMugnier@users.noreply.github.com>
2025-02-19 22:28:56 +0100

    feat(epic): build frame

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from 'react';
+import './App.css';
+
+interface IpfsContentFile {
+  images_collected_at: string;
+  asset_ipfs_hash: string;
+  images: { thumbnail: string; url: string; }[];
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const hasFetchedData = useRef(false);
+
+  useEffect(() => {
+
+    if (hasFetchedData.current) return;
+
+    hasFetchedData.current = true;
+
+    const fetchData = async (): Promise<void> => {
+      try {
+        console.log("Fetching data from IPFS");
+
+        const ipfsHash: string = "Qmcw111vvxnrwXjFft5dpj1dXJD4Lr1TX26qPLFXt9ggTY";
+        const response: Response = await fetch("https://ipfs.io/ipfs/" + ipfsHash);
+        const data: IpfsContentFile = await response.json();
+
+        const mainImageUrl: string = 'https://ipfs.io/ipfs/' + data.asset_ipfs_hash;
+        const mainImage: HTMLImageElement = document.getElementById("main-image") as HTMLImageElement;
+        mainImage.src = mainImageUrl;
+
+        const mainImageCaption: HTMLElement = document.getElementById('main-image-caption') as HTMLElement;
+        if (mainImageCaption) {
+          mainImageCaption.innerHTML = data.images_collected_at;
+        }
+
+        const thumbnailsContainer: HTMLDivElement = document.getElementById("thumbnails") as HTMLDivElement;
+
+        const img: HTMLImageElement = document.createElement("img");
+        img.src = mainImageUrl;
+        img.className = "thumbnail";
+        img.onclick = () => {
+          mainImage.src = mainImageUrl;
+        };
+        thumbnailsContainer?.appendChild(img);
+
+        data.images.forEach((image: { thumbnail: string; url: string; }) => {
+          const img: HTMLImageElement = document.createElement("img");
+          img.src = image.thumbnail;
+          img.className = "thumbnail";
+          img.onclick = () => {
+              mainImage.src = image.url;
+          };
+          thumbnailsContainer?.appendChild(img);
+        });
+
+      } catch (error) {
+        console.error("Erreur lors du chargement du JSON:", error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <section id="carousel-container">
+          <figure>
+            <img id="main-image" src="null" alt="Nasa Earth Polychromatic Imaging Camera" />
+            <figcaption id="main-image-caption"></figcaption>
+          </figure>
+          <div id="thumbnails"></div>
+        </section>
+
+        <section id="about">
+          <p>
+            Photos have been collected by Nasa DSCOVR's Earth Polychromatic Imaging Camera (EPIC) instrument. Uniquely positioned at the Earth-Sun Lagrange point, EPIC provides full disc imagery of the Earth and captures unique perspectives of certain astronomical events such as lunar transits using a 2048x2048 pixel CCD (Charge Coupled Device) detector coupled to a 30-cm aperture Cassegrain telescope.
+          </p>
+        </section>
+
+        <footer className="footer">
+          <p>&copy; 2025 - anyvoid.eth - See on <a href="https://github.com/NicolasMugnier/nasa-latest-epic">GitHub</a></p>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import FrameSDK from '@farcaster/frame-sdk'
-import { useEffect } from 'react'
-
-import { ReactNode } from 'react'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import FrameSDK from '@farcaster/frame-sdk';
+import { useEffect, ReactNode } from 'react';
 
 function FarcasterFrameProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
     const init = async () => {
       setTimeout(() => {
         FrameSDK.actions.ready()
-      }, 500)
-    }
-    init()
-  }, [])
-  return <>{children}</>
+      }, 500);
+    };
+    init();
+  }, []);
+  return <>{children}</>;
 }
 
 createRoot(document.getElementById('root')!).render(
